405fec66cc94f449c1cc84f82c40bdecf7601cda
Calvin <rempel.calvin@gmail.com>
2014-05-05 16:21:58 -0700

    Added expired verification exception. Renamed account deleted exception. Started on account verification.

--- php_inc/common.php
+++ php_inc/common.php
@@ -2,5 +2,6 @@
 define( 'DB_HOST', 'localhost' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASS', 'password' );
+define( 'VERIFICATION_EXPIRATION', 36000 );
 
 $db = new PDO( 'mysql:host=' . DB_HOST . ';dbname=StudyBuddy;charset=utf8', DB_USER, DB_PASS);
