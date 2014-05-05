b98d8865d85323ef1ef635098fd2f5b88acaa00d
mvouve <mvouve@gmail.com>
2014-05-05 15:05:09 -0700

    Update Account Settings
    Added function to update account settings, information should be passed
    in through post, uses same variable names as AJAX ENDPOINTS document.
    
    Cant be tested until helper functions are built.

--- php_inc/user/class-User.php
+++ php_inc/user/class-User.php
@@ -1,9 +1,48 @@
+<?php
 class User
 {
-	updateSettings()
+	/* user athentification settings */
+	private $userAuth;
+
+	/* constructor for User */
+	function __construct( $userAuth )
 	{
+		$this->userAuth = $userAuth;
 	}
-	isLoggedIn()
+	
+	/* function to update display name and password */
+	function updateSettings()
 	{
+		global $db;
+		
+		// unwrap $_POST variables.
+		extract( $_POST );
+		
+		$sql = 'UPDATE INTO User SET displayName = :display_name WHERE email = :email';
+		$sql = $db->prepare( $sql );
+		$sql->bindParam( ':email', $email );
+		$sql->bindParam( ':display_name', $displayName );
+		
+		
+		
+		// verify password change and update password.
+		if( $change_password == 'yes'
+		 && $userAuth->checkCredentials( $email, $old_pass )
+		 && $new_pass == $confirm_pass )
+		{
+			$userAuth->changePassword( $email, $new_pass );
+		}
+		
+		// verify valid displayname and update in the database.
+		if ( InputValidation::isValidDisplayName( $display_name ) )
+		{
+			$sql->execute();
+		}
+		
 	}
+	function isLoggedIn()
+	{
+		return true;
+	}
+	
 }
