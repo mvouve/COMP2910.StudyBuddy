ca27cbb1e5257d1204970dc6fe9e03f9f3b0f202
mvouve <mvouve@gmail.com>
2014-05-05 16:19:42 -0700

    Updated display name regex

--- php_inc/class-InputValidation.php
+++ php_inc/class-InputValidation.php
@@ -7,7 +7,7 @@ class InputValidation
 	static function isValidDisplayName( $displayName )
 	{
 		// define regular expression
-		$displayNameRegex = '/[0-9A-Za-z-/._]{5,}/g';
+		$displayNameRegex = '/([0-9A-Za-z-/._]{5,}$)/g';
 		
 		//check if displayname matches regular expression
 		if( !preg_match( $displayNameRegex, $displayName )
@@ -19,4 +19,3 @@ class InputValidation
 	}
 
 }
-	
