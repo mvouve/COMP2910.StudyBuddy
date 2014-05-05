06d3c919776e9ff6650a3edf1a0d33c306e571f7
mvouve <mvouve@gmail.com>
2014-05-05 16:24:17 -0700

    Added password regex, updated displayname regex

--- php_inc/class-InputValidation.php
+++ php_inc/class-InputValidation.php
@@ -7,10 +7,24 @@ class InputValidation
 	static function isValidDisplayName( $displayName )
 	{
 		// define regular expression
-		$displayNameRegex = '/([0-9A-Za-z-/._]{5,}$)/g';
+		$displayNameRegex = '/(^[0-9A-Za-z-/._]{5,32}$)/g';
 		
 		//check if displayname matches regular expression
 		if( !preg_match( $displayNameRegex, $displayName )
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	static function isValidPassword( $password )
+	{
+		// define regular expression
+		$passwordRegex = '/(^[0-9A-Za-z-/._]{5,50}$)/g';
+		
+		//check if displayname matches regular expression
+		if( !preg_match( $passwordRegex, $password )
 		{
 			return false;
 		}
