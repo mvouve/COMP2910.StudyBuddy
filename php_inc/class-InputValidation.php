8661c7d5518d0a49c53ea4313f7051cc05e9fc39
mvouve <mvouve@gmail.com>
2014-05-06 10:28:52 -0700

    Added password auth, added comments to PHP
    fixed leading spaces, added comments to class-User and
    class-InputValidation.

--- php_inc/class-InputValidation.php
+++ php_inc/class-InputValidation.php
@@ -1,35 +1,65 @@
 <?php
 class InputValidation
 {
-	/*
-	 * Ensures display name is valid.
-	 */
-	static function isValidDisplayName( $displayName )
-	{
-		// define regular expression
-		$displayNameRegex = '/(^[0-9A-Za-z-/._]{5,32}$)/g';
-		
-		//check if displayname matches regular expression
-		if( !preg_match( $displayNameRegex, $displayName )
-		{
-			return false;
-		}
-		
-		return true;
-	}
-	
-	static function isValidPassword( $password )
-	{
-		// define regular expression
-		$passwordRegex = '/(^[0-9A-Za-z-/._]{5,50}$)/g';
-		
-		//check if displayname matches regular expression
-		if( !preg_match( $passwordRegex, $password )
-		{
-			return false;
-		}
-		
-		return true;
-	}
+    /* 
+     * Check if input password is valid.
+     *
+     * @params $displayName the display name to be checked
+     *
+     * @returns true on valid, false on invalid 
+     */
+    static function isValidDisplayName( $displayName )
+    {
+        // define regular expression
+        $displayNameRegex = '/(^[0-9A-Za-z-/._]{5,32}$)/g';
+        
+        //check if displayname matches regular expression
+        if( !preg_match( $displayNameRegex, $displayName )
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    /* 
+     * Check if input password is valid.
+     *
+     * @params $password the password to be checked
+     *
+     * @returns true on valid, false on invalid 
+     */
+    static function isValidPassword( $password )
+    {
+        // define regular expression
+        $passwordRegex = '/(^[0-9A-Za-z-/._]{5,50}$)/g';
+        
+        //check if password matches regular expression
+        if( !preg_match( $passwordRegex, $password )
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    /* 
+     * Check if input email is valid. Valid emails must end in @my.bcit.ca
+     *
+     * @params $email the email to be checked
+     *
+     * @returns true on valid, false on invalid 
+     */
+    static function isValidPassword( $email )
+    {
+        // define regular expression
+        $emailRegex = '/(@my.bcit.ca$)/g';
+        
+        //check if email matches regular expression
+        if( !preg_match( $emailRegex, $email )
+        {
+            return false;
+        }
+        
+        return true;
+    }
 
 }
--- php_inc/user/class-User.php
+++ php_inc/user/class-User.php
@@ -1,51 +1,50 @@
 <?php
-/*
-StudyBuddy User class
-*/
 class User
 {
-	/* user athentification settings */
-	private $userAuth;
+    /* user athentification settings */
+    private $userAuth;
 
-	/* constructor for User */
-	function __construct( $userAuth )
-	{
-		$this->userAuth = $userAuth;
-	}
-	
-	/* function to update display name and password */
-	function updateSettings()
-	{
-		global $db;
-		
-		// unwrap $_POST variables.
-		extract( $_POST );
-		
-		$sql = 'UPDATE INTO User SET displayName = :display_name WHERE email = :email';
-		$sql = $db->prepare( $sql );
-		$sql->bindParam( ':email', $email );
-		$sql->bindParam( ':display_name', trim( $displayName ) );
-		
-		
-		
-		// verify password change and update password.
-		if( $change_password == 'yes'
-		 && $userAuth->checkCredentials( $email, $old_pass )
-		 && $new_pass == $confirm_pass )
-		{
-			$userAuth->changePassword( $email, $new_pass );
-		}
-		
-		// verify valid displayname and update in the database.
-		if ( InputValidation::isValidDisplayName( trim( $display_name ) )
-		{
-			$sql->execute();
-		}
-		
-	}
-	function isLoggedIn()
-	{
-		return true;
-	}
-	
+    /* constructor for User */
+    function __construct( $userAuth )
+    {
+        $this->userAuth = $userAuth;
+    }
+    
+    /* 
+     * function to update display name and password 
+     */
+    function updateSettings()
+    {
+        global $db;
+        
+        // unwrap $_POST variables.
+        extract( $_POST );
+        
+        $sql = 'UPDATE INTO User SET displayName = :display_name WHERE email = :email';
+        $sql = $db->prepare( $sql );
+        $sql->bindParam( ':email', $email );
+        $sql->bindParam( ':display_name', trim( $displayName ) );
+        
+        
+        
+        // verify password change and update password.
+        if( $change_password == 'yes'
+         && $userAuth->checkCredentials( $email, $old_pass )
+         && $new_pass == $confirm_pass )
+        {
+            $userAuth->changePassword( $email, $new_pass );
+        }
+        
+        // verify valid displayname and update in the database.
+        if ( InputValidation::isValidDisplayName( trim( $display_name ) )
+        {
+            $sql->execute();
+        }
+        
+    }
+    function isLoggedIn()
+    {
+        return true;
+    }
+    
 }
