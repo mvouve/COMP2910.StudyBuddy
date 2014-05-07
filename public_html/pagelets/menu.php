0985a77a980b4f9eeb71c21563c4230d99c9eaf1
Georgi Hristov <hristov798@gmail.com>
2014-05-07 11:43:10 -0700

    Created banner and menu pagelets and modified any affected files.

---
+++ public_html/pagelets/menu.php
@@ -0,0 +1,13 @@
+            <div data-role='panel' id='menuPanel' data-theme='b' data-position='right' data-display='overlay'>
+                <div class='panel-content'>
+                    <h1>Menu</h1><br>
+                    <ul data-role='listview'>
+                        <li><a href='#'>My Meetings</a></li>
+                        <li><a href='#'>Create Meetings</a></li>
+                        <li><br></li>
+                        <li><a href='#'>My Courses</a></li>
+                        <li><a href='#'>Account Settings</a></li>
+                        <li><a href='#'>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
