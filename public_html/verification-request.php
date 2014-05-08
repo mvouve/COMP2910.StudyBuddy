febe8c88d251d9d86ad87b8c4b54cf56dae33c71
a00880208 <alam160@my.bcit.ca>
2014-05-08 11:30:01 -0700

    Added buttons

--- public_html/verification-request.php
+++ public_html/verification-request.php
@@ -7,7 +7,24 @@
 			<div class="contenta center" data-role="content">
 				<img src="images/mail.png" alt="Check">
 				<br>Your verification email has been sent. Please check your e-mail.
-                <a href="my.bcit.ca" data-role="button" data-mini="true">MyBcit</a>
+                <div data-role="controlgroup" data-type="horizontal">
+                    <a href="http://my.bcit.ca" data-role="button">my.bcit.ca</a>
+                    <a href="http://bcit.ca" data-role="button">bcit.ca</a>
+                    <a href="http://learn.bcit.ca" data-role="button">learn.bcit.ca</a>
+                </div>
+
+
+
+                </br>
+                <div data-role="Verification">
+                    <form id="name-change" name="name-change" method="POST">
+                        <label for="display-name">Please enter your verification code:</label>
+                        <input type="text" name="display-name" id="display-name">
+                        <input id="update-name" type="button" value="Submit">
+                        <input type="hidden" name="method" value="update-display-name" />
+                    </form>
+                </div>
+                                
 			</div>
 		</div>
 	</body>
