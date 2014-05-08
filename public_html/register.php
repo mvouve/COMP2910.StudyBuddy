0f610e1c01fc72a2a30c16aafde53e75ed9a8686
Georgi Hristov <hristov798@gmail.com>
2014-05-07 19:59:06 -0700

    Changed names to conform to naming conventions.

--- public_html/register.php
+++ public_html/register.php
@@ -5,15 +5,15 @@
         <div data-role="page" data-theme="a">
             <?php renderPagelet( 'banner.php', array( '{{title}}' => 'Register Account' ) ); ?>
 			<div class="contenta" data-role="content" id="register">
-				<form id="registerForm" name="registerForm" method="POST">
-					<label for="email" id="emailLabel">Email:</label>
+				<form id="register-form" name="register-form" method="POST">
+					<label for="email" id="email-label">Email:</label>
 					<input type="text" name="email" id="email"><br/>
-					<label for="displayName" id="displayNameLabel">Display Name:</label>
-					<input type="text" name="displayName" id="displayName"><br/>
-					<label for="password" id="passwordLabel">Password:</label>
+					<label for="display-name" id="display-name-label">Display Name:</label>
+					<input type="text" name="display-name" id="display-name"><br/>
+					<label for="password" id="password-label">Password:</label>
 					<input type="password" name="password" id="password"><br/>
-					<label for="confirm" id="confirmLabel">Confirm Password:</label>
-					<input type="password" name="confirm_password" id="confirm"><br/>
+					<label for="confirm" id="confirm-label">Confirm Password:</label>
+					<input type="password" name="confirm-password" id="confirm"><br/>
                     <input id="register-submit" type="submit" value="Register">
                     <input type="hidden" name="method" value="register" />
 				</form>
@@ -25,7 +25,7 @@
             //used to ensure a user-entered email is a valid BCIT e-mail
             function validateEmail() {
                 var emailRegex = /^(([0-9a-z_.]+@((my\.bcit\.ca)|(bcit.ca)))|(a\d{8}@((mybcit\.ca)|(learn\.bcit\.ca))))$/gi;
-                var emailLabel = document.getElementById("emailLabel");
+                var emailLabel = document.getElementById("email-label");
                 var validEmail = document.getElementById("email").value.match(emailRegex);
                 if (validEmail == null || validEmail.length != 1) {
                     emailLabel.style.color="#FF0000";
@@ -38,8 +38,8 @@
             //used to ensure a user-entered display name is not null or empty
             function validateDisplayName() {
                 var displayNameRegex = /^[0-9A-Za-z-]{5,32}$/g;
-                var displayNameLabel = document.getElementById("displayNameLabel");
-                var displayName = document.getElementById("displayName").value.match(displayNameRegex);
+                var displayNameLabel = document.getElementById("display-name-label");
+                var displayName = document.getElementById("display-name").value.match(displayNameRegex);
                 if (displayName == null || displayName.length != 1) {
                     displayNameLabel.style.color="#FF0000";
                     return false;
@@ -50,8 +50,8 @@
 
             function validatePassword() {
                 var passwordRegex = /^.+$/g;
-                var passwordLabel = document.getElementById("passwordLabel");
-                var confirmLabel = document.getElementById("confirmLabel");
+                var passwordLabel = document.getElementById("password-label");
+                var confirmLabel = document.getElementById("confirm-label");
                 var password = document.getElementById("password").value.match(passwordRegex);
                 var confirm = document.getElementById("confirm").value;
                 if (password == null || password.length != 1) {
@@ -76,7 +76,7 @@
             }
             
             $("#email").keyup( function(e){validateEmail();} );
-            $("#displayName").keyup( function(e){validateDisplayName();} );
+            $("#display-name").keyup( function(e){validateDisplayName();} );
             $("#password").keyup( function(e){validatePassword();} );
             $("#confirm").keyup( function(e){validatePassword();} );
 
@@ -86,7 +86,7 @@
                 e.preventDefault();
                 if( !validateEmail() )
                 {
-                    alert("Invalid email!");
+                    alert("Invalid Email!");
                     return;
                 }
                 if( !validateDisplayName() )
@@ -99,7 +99,7 @@
                     alert("Invalid Password pair!");
                     return;
                 }
-                var formData = $("#registerForm").serializeArray();
+                var formData = $("#register-form").serializeArray();
 
                 $.post( <?php echo '\''.AJAX_URL . 'user/auth.php\''; ?>,
                         formData,
