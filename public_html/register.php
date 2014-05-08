5d3e9acd9b370dc8bee9e4be9b3b3c373babff7e
Georgi Hristov <hristov798@gmail.com>
2014-05-07 18:47:02 -0700

    Added dynamic input validation.

--- public_html/register.php
+++ public_html/register.php
@@ -6,15 +6,15 @@
             <?php renderPagelet( 'banner.php', array( '{{title}}' => 'Register Account' ) ); ?>
 			<div class="contenta" data-role="content" id="register">
 				<form id="registerForm" name="registerForm" method="POST">
-					<label for="email">E-mail:</label>
+					<label for="email" id="emailLabel">Email:</label>
 					<input type="text" name="email" id="email"><br/>
-					<label for="displayName">Display Name:</label>
+					<label for="displayName" id="displayNameLabel">Display Name:</label>
 					<input type="text" name="displayName" id="displayName"><br/>
 					<label for="password">Password:</label>
 					<input type="password" name="password" id="password"><br/>
 					<label for="confirm">Confirm Password:</label>
 					<input type="password" name="confirm_password" id="confirm"><br/>
-                    <input id="register-submit" type="submit" value="Register" onclick="register()">
+                    <input id="register-submit" type="button" value="Register">
                     <input type="hidden" name="method" value="register" />
 				</form>
 			</div>
@@ -22,35 +22,33 @@
 			</div>
 		</div>
         <script>
-            function register()
-            {
-                alert('Displayname: ' + (validateUsername()?'true':'false'));
+            var valid = false;
+            //used to ensure a user-entered email is a valid BCIT e-mail
+            function validateEmail() {
+                var emailRegex = /^[0-9a-z_.]@my\\.bcit\\.ca$/gi;
+                var emailLabel = document.getElementById("emailLabel");
+                var validEmail = document.getElementById("email").value.match(emailRegex);
+                if (validEmail == null || validEmail.length != 1) {
+                    emailLabel.style.color="#FF0000";
+                    valid = false;
+                    return false;
+                }
+                emailLabel.style.color="#00FF00";
+                valid &= true;
+                return true;
             }
 
             //used to ensure a user-entered display name is not null or empty
-            function validateUsername() {
-                var displayNameRegex = /[0-9A-Za-z-]{5,32}$/g;
-                var username = document.getElementById("displayName").value.match(displayNameRegex);
-                if ((username == null) || (username == "")) {
-                    alert("Please enter a user display name, 5-32 alphaneumeric characters.");
+            function validateDisplayName() {
+                var displayNameRegex = /^[0-9A-Za-z-]{5,32}$/g;
+                var displayNameLabel = document.getElementById("displayNameLabel");
+                var displayName = document.getElementById("displayName").value.match(displayNameRegex);
+                if (displayName == null || displayName.length != 1) {
+                    displayNameLabel.style.color="#FF0000";
                     return false;
                 }
+                displayNameLabel.style.color="#00FF00";
                 return true;
-            }
-
-            //used to ensure a user-entered email is a valid BCIT e-mail
-            function validateMail() {
-                var emailRegex = /@my.bcit.ca$/g;
-                var validEmail = document.form["loginForm"]["email"].value.match(emailRegex);
-                if (validEmail == null) {
-                    alert("must use a @my.bcit.ca e-mail address.");
-                    return false;
-                }
-                return true;
-            }
-
-            function checkEmail() {
-
             }
 
             function onRegister(result) {
@@ -59,10 +57,17 @@
                 // Reset the Submit button to inactive after being pressed
                 $.mobile.activePage.find('.ui-btn-active').removeClass('ui-btn-active ui-focus');
             }
+            
+            $("#email").keyup( function(e){validateEmail();} );
+            $("#displayName").keyup( function(e){validateDisplayName();} );
 
             // Note the change from $().click to $().on( 'click tap', function( e ) {} );
-            $("#register-submit").on('click tap', function (e) {
-
+            $("#register-submit").on( 'click tap', function (e) {
+                if( !validateEmail() || !validateDisplayName() )
+                {
+                    alert("Invalid email or displayName");
+                    return;
+                }
                 var formData = $("#registerForm").serializeArray();
 
                 $.post( <?php echo '\''.AJAX_URL . 'user/auth.php\''; ?>,
