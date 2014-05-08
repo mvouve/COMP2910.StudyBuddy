780a2cda9969d1d8182f106c8b96ae4432693736
Calvin <rempel.calvin@gmail.com>
2014-05-08 16:49:05 -0700

    -added logout to login page

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
-require( 'config.php' );
-require( PHP_INC_PATH . 'common.php' );
+require_once( 'config.php' );
+require_once( PHP_INC_PATH . 'common.php' );
 
 global $user;
 
--- public_html/login.php
+++ public_html/login.php
@@ -1,5 +1,7 @@
 <!--Study Buddy - Login Page-->
 <?php require_once( 'config.php' ); ?>
+<?php require_once( PHP_INC_PATH . 'common.php' ); ?>
+<?php User::instance()->logout(); ?>
 <?php renderPagelet( 'header.php', array( '{{customHeadTags}}' => '' ) ); ?>
 	<body>
 		<div data-role="page" data-theme="a">
