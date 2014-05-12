4475b3bad77f5927230287986d46c7795b273e1c
mvouve <mvouve@gmail.com>
2014-05-12 14:47:04 -0700

    added courses endpoint.

--- courses.sql
+++ courses.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Courses
+CREATE TABLE Course
 (
     ID      CHAR(8)     NOT NULL,
     name    VARCHAR(20) NOT NULL,
-    PRIMARY KEY( id )
+    PRIMARY KEY( ID )
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 CREATE TABLE UserCourse
@@ -10,5 +10,5 @@ CREATE TABLE UserCourse
     userID      INTEGER         NOT NULL,
     courseID    CHAR(8)         NOT NULL,
     visable     enum( 'T','F' ) NOT NULL    DEFAULT 'T',
-    PRIMARY KEY( id )
+    PRIMARY KEY( userID, courseID )
 ) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
---
+++ public_html/ajax/user/courses.php
@@ -0,0 +1,45 @@
+<?php
+require( '../../config.php' );
+require( PHP_INC_PATH . 'common.php' );
+
+if( isset( $_POST['method'] ) )
+{
+    //Add course function
+    switch( $_POST['method'] )
+    {
+        case 'add-course':
+            $retval = addCourse( $_POST['id'], $_POST['title'] );
+            break;
+        case 'get-courses':
+            $retval = getCourse();
+        
+        default:
+    }
+    return $retval;
+}
+
+/*
+ * Add a course to the database.
+ *
+ * @param id the ID of the course
+ * @param title 
+ * @return true | false
+ */
+function addCourse( $id, $title )
+{
+    global $courses;
+    $retval = array( 'success' => false );
+    
+    if ( $courses->createCourse( $id, $title ) )
+    {
+        $retval = array( 'success' => true );
+    }
+    
+    return $retval;
+
+}
+
+/*
+ *
+ *
+ */
