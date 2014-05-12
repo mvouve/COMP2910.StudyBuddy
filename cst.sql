899ce0cfcc33f56d3251b1945828999a523e3c72
Calvin <rempel.calvin@gmail.com>
2014-05-12 14:55:14 -0700

    -Changed CourseID's to 8 chars. -Updating CourseList to indicate user enrollment.

--- cst.sql
+++ cst.sql
@@ -2,136 +2,136 @@
 
 /* SEMESTER 1 */
 INSERT INTO Courses
-VALUES ('BUSA 2720', 'Business in a Networked Economy');
+VALUES ('BUSA2720', 'Business in a Networked Economy');
 
 INSERT INTO Courses
-VALUES ('COMM 1116', 'Business Communications 1');
+VALUES ('COMM1116', 'Business Communications 1');
 
 INSERT INTO Courses
-VALUES ('COMP 1100', 'CST Program Fundamentals');
+VALUES ('COMP1100', 'CST Program Fundamentals');
 
 INSERT INTO Courses
-VALUES ('COMP 1111', 'Essential Skills of Computing');
+VALUES ('COMP1111', 'Essential Skills of Computing');
 
 INSERT INTO Courses
-VALUES ('COMP 1113', 'Applied Mathematics');
+VALUES ('COMP1113', 'Applied Mathematics');
 
 INSERT INTO Courses
-VALUES ('COMP 1510', 'Programming Methods');
+VALUES ('COMP1510', 'Programming Methods');
 
 INSERT INTO Courses
-VALUES ('COMP 1536', 'Introduction to Web Development');  
+VALUES ('COMP1536', 'Introduction to Web Development');  
 
 
 /* SEMESTER 2 */
 INSERT INTO Courses
-VALUES ('COMM 2216', 'Business Communications 2');
+VALUES ('COMM2216', 'Business Communications 2');
 
 INSERT INTO Courses
-VALUES ('COMP 2121', 'Discrete Mathematics');
+VALUES ('COMP2121', 'Discrete Mathematics');
 
 INSERT INTO Courses
-VALUES ('COMP 2510', 'Procedural Programming in C');
+VALUES ('COMP2510', 'Procedural Programming in C');
 
 INSERT INTO Courses
-VALUES ('COMP 2526', 'Object-Oriented Programming in Java');
+VALUES ('COMP2526', 'Object-Oriented Programming in Java');
 
 INSERT INTO Courses
-VALUES ('COMP 2714', 'Relational Database Systems');
+VALUES ('COMP2714', 'Relational Database Systems');
 
 INSERT INTO Courses
-VALUES ('COMP 2721', 'Computer Organization / Architecture');
+VALUES ('COMP2721', 'Computer Organization / Architecture');
 
 INSERT INTO Courses
-VALUES ('COMP 2910', 'Projects');
+VALUES ('COMP2910', 'Projects');
 
 /* SEMESTER 3 */
 INSERT INTO Courses
-VALUES ('COMP 3512', 'Object Oriented Programming in C++');
+VALUES ('COMP3512', 'Object Oriented Programming in C++');
 
 INSERT INTO Courses
-VALUES ('COMP 3711', 'Object Oriented Analysis and Design');
+VALUES ('COMP3711', 'Object Oriented Analysis and Design');
 
 INSERT INTO Courses
-VALUES ('COMP 3721', 'Introduction to Data Communications');
+VALUES ('COMP3721', 'Introduction to Data Communications');
 
 INSERT INTO Courses
-VALUES ('COMP 3760', 'Algorithm Analysis and Design');
+VALUES ('COMP3760', 'Algorithm Analysis and Design');
 
 INSERT INTO Courses
-VALUES ('COMP 3900', 'Computer Projects Practicum 1');
+VALUES ('COMP3900', 'Computer Projects Practicum 1');
 
 /* SEMESTER 3 OPTION CLASSES */
 INSERT INTO Courses
-VALUES ('COMP 3940', 'Client/Server Computing 1');
+VALUES ('COMP3940', 'Client/Server Computing 1');
 
 INSERT INTO Courses
-VALUES ('COMP 3920', 'Database Systems 1');
+VALUES ('COMP3920', 'Database Systems 1');
 
 INSERT INTO Courses
-VALUES ('COMP 3980', 'Data Communications/Internetworking 1');
+VALUES ('COMP3980', 'Data Communications/Internetworking 1');
 
 INSERT INTO Courses
-VALUES ('COMP 3931', 'Digital Image, Video, and Audio Fundamentals');
+VALUES ('COMP3931', 'Digital Image, Video, and Audio Fundamentals');
 
 INSERT INTO Courses
-VALUES ('COMP 3910', 'Information Technology Management');
+VALUES ('COMP3910', 'Information Technology Management');
 
 INSERT INTO Courses
-VALUES ('COMP 3951', 'Technical Programming 1');
+VALUES ('COMP3951', 'Technical Programming 1');
 
 /* SEMESTER 4 */
 INSERT INTO Courses
-VALUES ('BLAW 3600', 'Computers and the Law');
+VALUES ('BLAW3600', 'Computers and the Law');
 
 INSERT INTO Courses
-VALUES ('COMP 3717', 'Mobile Application Development with Android');
+VALUES ('COMP3717', 'Mobile Application Development with Android');
 
 INSERT INTO Courses
-VALUES ('COMP 4100', 'Career Preparation');
+VALUES ('COMP4100', 'Career Preparation');
 
 INSERT INTO Courses
-VALUES ('COMP 4560', 'Computer Graphics for Computer Systems Technology');
+VALUES ('COMP4560', 'Computer Graphics for Computer Systems Technology');
 
 INSERT INTO Courses
-VALUES ('COMP 4711', 'Introduction to Internet Software Development');
+VALUES ('COMP4711', 'Introduction to Internet Software Development');
 
 INSERT INTO Courses
-VALUES ('COMP 4735', 'Operating Systems');
+VALUES ('COMP4735', 'Operating Systems');
 
 INSERT INTO Courses
-VALUES ('COMP 4900', 'Computer Projects Practicum');
+VALUES ('COMP4900', 'Computer Projects Practicum');
 
 /* SEMESTER 4 OPTION CLASSES */
 INSERT INTO Courses
-VALUES ('COMP 4941', 'Client/Server Computing 2');
+VALUES ('COMP4941', 'Client/Server Computing 2');
 
 INSERT INTO Courses
-VALUES ('COMP 4945', 'Special Topics in Client/Server');
+VALUES ('COMP4945', 'Special Topics in Client/Server');
 
 INSERT INTO Courses
-VALUES ('COMP 4921', 'Database Systems 2');
+VALUES ('COMP4921', 'Database Systems 2');
 
 INSERT INTO Courses
-VALUES ('COMP 4981', 'Data Communications/Internetworking 2');
+VALUES ('COMP4981', 'Data Communications/Internetworking 2');
 
 INSERT INTO Courses
-VALUES ('COMP 4985', 'Selected Topics in Communications/Internetworking 2');
+VALUES ('COMP4985', 'Selected Topics in Communications/Internetworking 2');
 
 INSERT INTO Courses
-VALUES ('COMP 4932', 'Advanced Topics in Digital Processing');
+VALUES ('COMP4932', 'Advanced Topics in Digital Processing');
 
 INSERT INTO Courses
-VALUES ('COMP 4995', 'Gaming Systems');
+VALUES ('COMP4995', 'Gaming Systems');
 
 INSERT INTO Courses
-VALUES ('COMP 4911', 'Managing IS Development');
+VALUES ('COMP4911', 'Managing IS Development');
 
 INSERT INTO Courses
-VALUES ('COMP 4915', 'Special Topics in MIS');
+VALUES ('COMP4915', 'Special Topics in MIS');
 
 INSERT INTO Courses
-VALUES ('COMP 4952', 'Technical Programming 2');
+VALUES ('COMP4952', 'Technical Programming 2');
 
 INSERT INTO Courses
-VALUES ('COMP 4956', 'System Programming');
+VALUES ('COMP4956', 'System Programming');
--- php_inc/class-course-manager.php
+++ php_inc/class-course-manager.php
@@ -2,6 +2,7 @@
 class CourseManager
 {
 	const COURSE_TABLE = 'Course';
+	const USER_COURSE_TABLE = 'UserCourse';
 
 	/*
 	 * Add a course to the database.
@@ -30,26 +31,110 @@ class CourseManager
 	/*
 	 * Get the entire Course List.
 	 *
+	 * @userID the ID of the User (null if not logged in)
 	 * @returns array( id, title ) of courses.
 	 */
-	public function getCourseList()
+	public function getCourseList( $userID )
 	{
 		global $db;
 		$retval = array();
+		$sql = '';
 		
-		$sql = 'SELECT *
-				FROM ' . CourseManager::COURSE_TABLE . ' 
+		if ( $user == null )
+		{
+			$sql = 'SELECT *
+					FROM ' . CourseManager::COURSE_TABLE . ' 
+					;';
+			$sql = $db->prepare( $sql );
+			$sql->execute();
+			$result = null;
+			
+			while ( ( $result = $db->fetch( PDO::FETCH_ASSOC ) ) != null )
+			{
+				$retval[] = array( 'id' => $result['ID'], 'title' => $result['name'] );
+			}
+		}
+		else
+		{
+			
+		}
+		
+		return $retval;
+	}
+	
+	/*
+	 * Add a Course to a User Courses List.
+	 *
+	 * @param $userID the the ID of the User.
+	 * @param $courseID the ID of the Course.
+	 *
+	 * @returns true on success, false on failure.
+	 */
+	public function addUserCourse( $userID, $courseID )
+	{
+		global $db;
+		
+		$sql = 'INSERT INTO ' . CourseManager::USER_COURSE_TABLE . ' 
+					(userID, courseID, visible)
+				VALUES
+					(:uid, :cID, \'F\')
 				;';
 		$sql = $db->prepare( $sql );
+		$sql->bindParam( ':uid', $userID );
+		$sql->bindParam( ':cID', $courseID );
+		
+		return $sql->execute();
+	}
+	
+	/*
+	 * Remove a Course from a User list.
+	 *
+	 * @param $userID the users ID
+	 * @param $courseID the course ID
+	 *
+	 * @returns true on success, false on failure.
+	 */
+	public function removeUserCourse( $userID, $courseID )
+	{
+		global $db;
+		
+		$sql = 'DELETE FROM ' . CourseManager::USER_COURSE_TABLE . ' 
+				WHERE userID=:uid
+					AND courseID=:cid
+				;';
+		$sql = $db->prepare( $sql );
+		
+		return $sql->execute();
+	}
+	
+	/*
+	 * Get the Course List of a specific User
+	 *
+	 * @param $userID the ID of the user whose list to retrieve.
+	 * 
+	 * @returns array( id, title ) of courses.
+	 */
+	public function getUserCourseList( $userID )
+	{
+		global $db;
+		
+		$sql = 'SELECT c.ID, c.name
+				FROM ' . CourseManager::COURSE_TABLE . ' c
+					JOIN ' . CourseManager::USER_COURSE_TABLE . ' uc
+						ON c.ID = uc.courseID
+				WHERE uc.userID=:uid
+				;';
+		$sql = $db->prepare( $sql );
+		$sql->bindParam( ':uid', $userID );
 		$sql->execute();
+		
 		$result = null;
 		
 		while ( ( $result = $db->fetch( PDO::FETCH_ASSOC ) ) != null )
 		{
-			$result[] = array( 'id' => $result['ID'], 'title' => $result['name'] );
+			$retval[] = array( 'id' => $result['ID'], 'title' => $result['name'] );
 		}
 		
-		return $result;
+		return $retval;
 	}
-	
 }
