218f9deddb0d774b4e4ed2c59d86b02a0ba3f0e6
Calvin <rempel.calvin@gmail.com>
2014-05-12 15:31:31 -0700

    Changed courses to course

--- cst.sql
+++ cst.sql
@@ -1,137 +1,137 @@
 ﻿/* This will populate the course list with CST courses in the BCIT curriculum */
 
 /* SEMESTER 1 */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('BUSA2720', 'Business in a Networked Economy');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMM1116', 'Business Communications 1');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP1100', 'CST Program Fundamentals');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP1111', 'Essential Skills of Computing');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP1113', 'Applied Mathematics');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP1510', 'Programming Methods');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP1536', 'Introduction to Web Development');  
 
 
 /* SEMESTER 2 */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMM2216', 'Business Communications 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2121', 'Discrete Mathematics');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2510', 'Procedural Programming in C');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2526', 'Object-Oriented Programming in Java');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2714', 'Relational Database Systems');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2721', 'Computer Organization / Architecture');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP2910', 'Projects');
 
 /* SEMESTER 3 */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3512', 'Object Oriented Programming in C++');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3711', 'Object Oriented Analysis and Design');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3721', 'Introduction to Data Communications');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3760', 'Algorithm Analysis and Design');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3900', 'Computer Projects Practicum 1');
 
 /* SEMESTER 3 OPTION CLASSES */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3940', 'Client/Server Computing 1');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3920', 'Database Systems 1');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3980', 'Data Communications/Internetworking 1');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3931', 'Digital Image, Video, and Audio Fundamentals');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3910', 'Information Technology Management');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3951', 'Technical Programming 1');
 
 /* SEMESTER 4 */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('BLAW3600', 'Computers and the Law');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP3717', 'Mobile Application Development with Android');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4100', 'Career Preparation');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4560', 'Computer Graphics for Computer Systems Technology');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4711', 'Introduction to Internet Software Development');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4735', 'Operating Systems');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4900', 'Computer Projects Practicum');
 
 /* SEMESTER 4 OPTION CLASSES */
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4941', 'Client/Server Computing 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4945', 'Special Topics in Client/Server');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4921', 'Database Systems 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4981', 'Data Communications/Internetworking 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4985', 'Selected Topics in Communications/Internetworking 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4932', 'Advanced Topics in Digital Processing');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4995', 'Gaming Systems');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4911', 'Managing IS Development');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4915', 'Special Topics in MIS');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4952', 'Technical Programming 2');
 
-INSERT INTO Courses
+INSERT INTO Course
 VALUES ('COMP4956', 'System Programming');
