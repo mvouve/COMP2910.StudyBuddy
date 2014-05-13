7d5762a3e275c36e3405bdc6c55ef9bd7f70bb0a
Georgi Hristov <hristov798@gmail.com>
2014-05-13 10:48:49 -0700

    Made changes to match all courses

--- public_html/my-courses.php
+++ public_html/my-courses.php
@@ -13,7 +13,7 @@
 	                <li data-icon="false"><a href="#">COMP1116<br>Business Communications 1</a></li>
 	                <li data-icon="false"><a href="#">COMP1100<br>CST Program Fundamentals</a></li>
 	                <li data-icon="false"><a href="#">COMP1111<br>Essential Skills for Computing</a></li>
-	                <li data-icon="check"><a href="#">COMP1113<br>Applied Mathematics</a></li>
+	                <li data-icon="false"><a href="#">COMP1113<br>Applied Mathematics</a></li>
     	            <li data-icon="false"><a href="#">COMP1510<br>Programming Methods</a></li>
     	            <li data-icon="false"><a href="#">COMP1536<br>Introduction to Web Development</a></li>
                 </ul>
@@ -21,8 +21,8 @@
             <div data-role="footer" data-position="fixed">
                 <div data-role="navbar">
 		            <ul>
-			            <li><a href="#">Add Courses</a></li>
-			            <li><a href="#">Remove Courses</a></li>
+			            <li><a href="#" id="add-course-button">Add Courses</a></li>
+			            <li><a href="#" id="remove-course-button">Remove Courses</a></li>
 		            </ul>
 	            </div>
             </div>
@@ -36,15 +36,24 @@
 
             function pupulateCourseList(result)
             {
-                var allCoursesList = document.getElementById('all-courses-list');
+                var myCoursesList = document.getElementById('my-courses-list');
                 for( var i = 0; i < result.length; ++i )
                 {
                     var newLI = document.createElement('li');
                     newLI.setAttribute( 'data-icon', (result[i].inCourse?'check':'false') );
                     newLI.innerHTML = '<a href="#">' + result[i].id + '<br>' + result[i].title + '</a>';
-                    allCoursesList.appendChild(newLI);
+                    myCoursesList.appendChild(newLI);
                 }
+
+				$('#my-courses-list').listview('refresh');
             }
+
+            $('#remove-course-button').on( 'click tap', function(e){
+                //alert('tapped');
+                $('#my-courses-list>li').attr('data-icon', 'delete');
+                $('#my-courses-list a').addClass('ui-btn-icon-right ui-icon-delete');
+				$('#my-courses-list').listview('refresh');
+            } )
         </script>
     </body>
 </html>
