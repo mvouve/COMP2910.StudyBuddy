d695157b89a80e48cf2ef813c8a9407c0088d10f
Calvin <rempel.calvin@gmail.com>
2014-05-13 10:49:40 -0700

    -Updated for new pages.

--- public_html/main.php
+++ public_html/main.php
@@ -15,26 +15,6 @@
     ');
 ?>
 <?php renderPagelet( 'header.php', $sliderHeader ); ?>
-
-<!--doctype html>
-<html>
-    <head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css" />
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css">
-		<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
-        <link rel="stylesheet" href="css/study-buddy-theme.min.css" />
-        <link rel="stylesheet" href="css/jquery.mobile.icons.min.css" />
-        <!-- stuff for panel->
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-        <script type="text/javascript" src="js/jquery.slidepanel.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/jquery.slidepanel.css">
-        <!-- /stuff for panel->
-
-        <link rel="stylesheet" href="css/custom.css">
-    </head>
--->
     <body>
         <div data-role="page" id="page" data-theme="a">
             <?php define('HAS_MENU',1);
@@ -43,5 +23,9 @@
                     <p>Main Page</p>
             </div>     
         </div>
+		
+		<?php include( 'all-courses.php' ); ?>
+		<?php include( 'my-courses.php' ); ?>
+		<?php include( 'add-course.php' ); ?>
     </body>
 </html>
