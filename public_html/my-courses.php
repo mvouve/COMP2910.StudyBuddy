2f80286ea4866ac79e0e32988b31b0b61c3c60dc
Calvin <rempel.calvin@gmail.com>
2014-05-13 15:01:40 -0700

    -fixed listview refresh

--- public_html/my-courses.php
+++ public_html/my-courses.php
@@ -31,6 +31,7 @@
             {
                 myCoursesList = document.getElementById('my-courses-list');
                 removeMode = false;
+				$('#my-courses-list').listview();
 
                 var getMyCoursesFormData = $("#get-my-courses-form").serializeArray();
                 $.post( <?php echo '\'' . AJAX_URL . 'courses/user-courses.php\''; ?>,
@@ -40,19 +41,19 @@
                             
                 $('#remove-course-button').on( 'click tap', function(e)
                 {
-                    //alert('tapped');
+                    alert('tapped');
                     if( removeMode )
                     {
                         $('#my-courses-list>li').attr('data-icon', 'false');
                         $('#my-courses-list a').removeClass('ui-btn-icon-right ui-icon-delete');
-				        //$('#my-courses-list').listview('refresh');
+				        $('#my-courses-list').listview('refresh');
                         $('#remove-course-button').html('Remove Courses');                    
                     }
                     else
                     {
                         $('#my-courses-list>li').attr('data-icon', 'delete');
                         $('#my-courses-list a').addClass('ui-btn-icon-right ui-icon-delete');
-				        //$('#my-courses-list').listview('refresh');
+				        $('#my-courses-list').listview('refresh');
                         $('#remove-course-button').html('Finish');
                     }
                     removeMode = !removeMode;
