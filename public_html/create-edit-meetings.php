4f046088e607f7faa2050c63c7ff324e19e1788c
spelka <sebastian.pelka@hotmail.com>
2014-05-14 12:20:38 -0700

    created skeleton

---
+++ public_html/create-edit-meetings.php
@@ -0,0 +1,14 @@
+<!-- create and edit meetings page-->
+<div data-role="page">
+    <div data-role="header">
+    </div>
+
+    <div data-role="main" id="create-edit-meeting-div">
+        <form id="create-edit-meeting-form" name="create-edit-meeting-form" method="post">
+               
+        </form>
+    </div>
+
+    <div data-role="footer">
+    </div>
+</div>
