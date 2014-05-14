168bd0839d194e395d419e2c30c7e032cc2ad9a5
spelka <sebastian.pelka@hotmail.com>
2014-05-14 13:54:15 -0700

    create and edit meetings HTML form started, must figure out date time

--- public_html/create-edit-meetings.php
+++ public_html/create-edit-meetings.php
@@ -5,7 +5,17 @@
 
     <div data-role="main" id="create-edit-meeting-div">
         <form id="create-edit-meeting-form" name="create-edit-meeting-form" method="post">
-               
+            <label for="course-dropdown">Course :</label>
+            <select id="course-dropdown" name="course-dropdown"></select>
+
+            <label for="location-dropdown">Location :</label>
+            <select id="location-dropdown" name="location-dropdown"></select>
+
+            <label for="meeting-date">Meeting Date:</label>
+            <input type="date" name="meeting-date">
+
+            <label for="meeting-start-time">Start Time:</label>
+
         </form>
     </div>
 
