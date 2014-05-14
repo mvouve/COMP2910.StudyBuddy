43af8320bfadd925dc4946254b6f21d4fbacaf95
spelka <sebastian.pelka@hotmail.com>
2014-05-13 20:04:43 -0700

    Filled in function skeletons

--- public_html/js/courses.js
+++ public_html/js/courses.js
@@ -17,11 +17,10 @@ function getUserCourses( ajax_URL )
             for ( i = 0 ; courseArray.length ; i++ )
             {
                 var courseID = courseArray[i].id;
-                var courseTitle = courseArray[i].title;
-                var userInCourse = courseArray[i].inCourse;
+                var courseTitle = courseArray[i].title; 
 
                 //calls a separate function to add this data to the HTML
-                courseListAdd(courseID, courseTitle, userInCourse);
+                userCourseListAdd(courseID, courseTitle);
             }
         }
     });
@@ -32,9 +31,9 @@ function getUserCourses( ajax_URL )
     @param id the 4-letter and 4-number course code
     @param title a brief description / the name of the course
     @param inCourse boolean, true if the user in the course*/
-function courseListAdd (id, title, inCourse)
+function userCourseListAdd (id, title)
 {
-    var list = getElementById("my-courses-list");
+    var list = getElementById('my-courses-list');
     var listItem = document.createElmeent('li');
 
     //create inner anchor element in list item and set its attribute and data
@@ -48,7 +47,7 @@ function courseListAdd (id, title, inCourse)
     //MUST STILL DEAL WITH IN_COURSE BOOLEAN; BUTTON IS NOT IMPLEMENTED AS OF WRITING
 }
 
-/* Fetch master course list from the serverajax
+/* Fetch master course list from the server
     @param ajax_URL the URI location where the ajax folder is located */
 function getCourseList( ajax_URL )
 {
@@ -57,17 +56,45 @@ function getCourseList( ajax_URL )
         url: ajax_URL + '/courses/courses.php',
         data:
         {
-            method: get-courses,
+            method: get-courses
         },
         dataType: json,
         success: function ( json )
         {
-            //PLACEHOLDER
+            var courseArray = json;
+            for ( i = 0 ; courseArray.length ; i++ )
+            {
+                var courseID = courseArray[i].id;
+                var courseTitle = courseArray[i].title;
+                var userInCourse = courseArray[i].inCourse;
+
+                //calls a separate function to add this data to the HTML
+                masterCourseListAdd(courseID, courseTitle, userInCourse);
         }
-    })
+    });
 }
 
-/* Add a course to the master course list ajax
+/* Adds course data to list elements in HTML 
+    @param id the 4-letter and 4-number course code
+    @param title a brief description / the name of the course
+    @param inCourse boolean, true if the user in the course*/
+function masterCourseListAdd (id, title, inCourse)
+{
+    var list = getElementById(/*PLACEHOLDER_FOR_LIST_ID*/);         //*****REPLACE PLACEHOLDER ID IN THIS LINE
+    var listItem = document.createElmeent('li');
+
+    //create inner anchor element in list item and set its attribute and data
+    var anchor = document.createElement('a');
+    anchor.setAttribute('href', '#');
+    anchor.innerHTML='' + id + '<br/>' + title;
+
+    //put the anchor element inside the list item element
+    listItem.innerHTML = anchor;
+
+    //MUST STILL DEAL WITH IN_COURSE BOOLEAN; BUTTON IS NOT IMPLEMENTED AS OF WRITING
+}
+
+/* Add a course to the master course list
     @param ajax_URL the URI location where the ajax folder is located
     @param courseID the 4-letter and 4-number course code
     @param description a brief description / the name of the course */
@@ -115,7 +142,7 @@ function addUserCourse( ajax_URL, courseID )
 /* removes a course from the user list 
     @param ajax_URL the URI location where the ajax folder is located
     @param courseID the 4-letter and 4-number course code */
-function removeCourse ( ajax_URL, courseID )
+function removeUserCourse ( ajax_URL, courseID )
 {
     $.ajax
     ({
