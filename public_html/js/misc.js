07732953ad249bfa042b14813027713e82ac484c
a00880208 <alam160@my.bcit.ca>
2014-05-15 18:49:27 -0700

    calvin halp

--- public_html/js/misc.js
+++ public_html/js/misc.js
@@ -1,23 +1,44 @@
 function colorChange()
 {
-    $( '.green-button' ).on( 'click tap',function (e){
-        $( '.ui-navbar' ).removeClass( 'black-ui-navbar' ).removeClass('purple-ui-navbar').removeClass('cb-ui-navbar').addClass( 'green-ui-navbar' );
-        $( '.listview-wrapper form' ).removeClass( 'black-listview-wrapper' ).removeClass( 'purple-listview-wrapper' ).removeClass('cb-listview-        wrapper').addClass( 'green-listview-wrapper' );
-    });
-
-    $( '.dark-button' ).on( 'click tap',function (e){
-        $( '.ui-navbar' ).removeClass( 'green-ui-navbar' ).removeClass('purple-ui-navbar').removeClass('cb-ui-navbar').addClass( 'black-ui-navbar' );
-        $( '.listview-wrapper form' ).removeClass( 'green-listview-wrapper' ).removeClass( 'purple-listview-wrapper' ).removeClass('cb-listview-wrapper').addClass( 'black-listview-wrapper' );
-        
-    });
-
-    $( '.purple-button' ).on( 'click tap',function (e){
-        $( '.ui-navbar' ).removeClass( 'green-ui-navbar' ).removeClass( 'black-ui-navbar' ).removeClass('cb-ui-navbar').addClass( 'purple-ui-navbar' );
-        $( '.listview-wrapper form' ).removeClass( 'black-listview-wrapper' ).removeClass( 'green-listview-wrapper' ).removeClass('cb-listview-wrapper').addClass( 'purple-listview-wrapper' );
-    });
+    var cookieStuff = readCookie('style');
+    if(cookieStuff != null){
+        if(cookieStuff.match("css/study-buddy-theme-5.min.css"))
+        {
+            purpleChange();
+        }
+        if(cookieStuff.match("css/StudyBuddyV2.css"))
+        {
+            greenChange();
+        }
+        if(cookieStuff.match("css/NightTheme.css"))
+        {
+            darkChange();
+        }
+        if(cookieStuff.match("css/ColorBlindOption.css"))
+        {
+            cbChange();
+        }
+    }
     
-    $( '.cb-button' ).on( 'click tap',function (e){
+    $( '.green-button' ).on( 'click tap',greenChange);
+    $( '.dark-button' ).on( 'click tap',darkChange);
+    $( '.purple-button' ).on( 'click tap',purpleChange);
+    $( '.cb-button' ).on( 'click tap', cbChange);
+    
+    function greenChange(){
+        $( '.ui-navbar' ).removeClass( 'black-ui-navbar' ).removeClass('purple-ui-navbar').removeClass('cb-ui-navbar').addClass( 'green-ui-navbar' );
+        $( '.listview-wrapper form' ).removeClass( 'black-listview-wrapper' ).removeClass( 'purple-listview-wrapper' ).removeClass( 'cb-listview-wrapper' ).addClass( 'green-listview-wrapper' );
+    }
+    function darkChange(){
+        $( '.ui-navbar' ).removeClass( 'green-ui-navbar' ).removeClass('purple-ui-navbar').removeClass('cb-ui-navbar').addClass( 'black-ui-navbar' );
+        $( '.listview-wrapper form' ).removeClass( 'green-listview-wrapper' ).removeClass( 'purple-listview-wrapper' ).removeClass( 'cb-listview-wrapper' ).addClass( 'black-listview-wrapper' );
+    }
+    function purpleChange(){
+        $( '.ui-navbar' ).removeClass( 'green-ui-navbar' ).removeClass( 'black-ui-navbar' ).removeClass('cb-ui-navbar').addClass( 'purple-ui-navbar' );
+        $( '.listview-wrapper form' ).removeClass( 'black-listview-wrapper' ).removeClass( 'green-listview-wrapper' ).removeClass( 'cb-listview-wrapper' ).addClass( 'purple-listview-wrapper' );
+    }
+    function cbChange(){
         $( '.ui-navbar' ).removeClass( 'green-ui-navbar' ).removeClass( 'black-ui-navbar' ).removeClass( 'purple-ui-navbar' ).addClass('cb-ui-navbar');
         $( '.listview-wrapper form' ).removeClass( 'black-listview-wrapper' ).removeClass( 'green-listview-wrapper' ).removeClass( 'purple-listview-wrapper' ).addClass( 'cb-listview-wrapper' );
-    });    
+    }
 }
