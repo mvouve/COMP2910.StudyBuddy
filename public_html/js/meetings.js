a6b672486d79854b287f1e38324088c0cbbf0516
spelka <sebastian.pelka@hotmail.com>
2014-05-19 09:50:06 -0700

    create meeting skeleton created. minor formatting changes

--- public_html/js/meetings.js
+++ public_html/js/meetings.js
@@ -1,14 +1,17 @@
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 /* a method to return all the meetings that you are attending.
-@param ajax_URL the URI where the ajax folder is located */
+@param ajax_URL the URI location where the ajax folder is located */
 
 function getMeetings(ajax_URL)
 {
     $.ajax
     ({
         url: ajax_URL + 'meetings/meetings.php',
-        method: get - meetings,
+        data:
+        {
+            method: 'get-meetings'
+        },
         success: function (json) {
             var meetingID = json.id;
             var meetingCreator = json.creatorID;
@@ -26,20 +29,37 @@ function getMeetings(ajax_URL)
     });
 }
    
-   /* 
-	method: get-meetings
-	Returns: array [ id = int,
-   creatorID = int,
-   courseID = string,
-   description = string,
-   location = string,
-   startTime = date,
-   endTime = date,
-   maxBuddies = int,
-   buddies = array[ displayName ],
-   attending = true | false
-  ]
-------------------------------------------------------------------------------------------------------------
+/* creates a new meeting
+    @param ajax_URL  the URI location where the ajax folder is located
+    @param courseID the course being studied at the meeting
+    @param CourseDescription a description of the course
+    @param meetingLocation the place where the meeting will be held
+    @param startTime a datetime string informing when the meeting begins (YYYY-MM-DD HH:MM:SS)
+    @param endTime a datetime string informing when the meeting ends (YYYY-MM-DD HH:MM:SS)
+    @param maxBuddies the maximum number of people that a location can accomidate */
+
+function createMeeting (ajax_URL, courseID, courseDescription, meetingLocation, startTime, endTime, maxBuddies)
+{
+    $.ajax
+    ({
+        url: ajax_URL + 'meetings/meetings.php',
+        data:
+        {
+            course-id: courseID,        //problem: javascript does not like a hyphen in course-id. Change this once Calvin changes the back end name, i guess.
+            description: courseDescription,
+            location: meetingLocation, 
+            start-time: startTime,
+            end-time: endTime,
+            max-buddies: maxBuddies
+        },
+        success: function (json)
+        {
+            //to do later
+        }
+    })
+}
+
+/*--------------------------------------------------------------------------------------------
 	method: create-meeting
 	course-id: string (eg. COMP1510)
 	description: string
@@ -48,7 +68,7 @@ function getMeetings(ajax_URL)
 	end-time: string
 	max-buddies: int
 	Returns: success: true | false
-------------------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------------------
 	method: edit-meeting
 	id: int
 	course-id: string
@@ -58,7 +78,7 @@ function getMeetings(ajax_URL)
 	end-time: string
 	max-buddies: int
 	Returns: success: true | false
-------------------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------------------
 	method: cancel-meeting
 	id: int
 	Returns: success: true | false
