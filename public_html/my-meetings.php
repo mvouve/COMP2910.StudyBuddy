8a4e214251925b545d113303dd805e7e2b1afd78
a00880208 <alam160@my.bcit.ca>
2014-05-20 13:44:34 -0700

    Please help with the skeleton for the js function

--- public_html/my-meetings.php
+++ public_html/my-meetings.php
@@ -11,13 +11,93 @@
         <div data-role="footer" data-position="fixed" data-tap-toggle="false">
             <div data-role="navbar">
                 <ul>
-                    <li><a href="#" data-icon="plus" data-iconpos="top">I created</a></li>
-                    <li><a href="#" data-icon="plus" data-iconpos="top">All meetings</a></li>
-                    <li><a href="#" data-icon="plus" data-iconpos="top">Meetings I'm Attending</a></li>
+                    <li><a href="#" data-icon="plus" data-iconpos="top" id="i-created">I created</a></li>
+                    <li><a href="#" data-icon="plus" data-iconpos="top" id="all-meeting">All meetings</a></li>
+                    <li><a href="#" data-icon="plus" data-iconpos="top" id="i-attending">Meetings I'm Attending</a></li>
                 </ul>
             </div>
         </div>
     </div>
-<!--End of my-meetings.php-->
-                        
+<script>
+    /*
+    $( '#i-created' ).on( 'click tap', function(e)
+        {
             
+        
+            var templist;
+            for(blabla)
+            {
+                if(beepboop.filter == '2')
+                {
+                    //add to list
+                }
+            }
+            //remove current list
+            //append new list
+            //refresh
+        });
+    $( '#all-meeting' ).on( 'click tap', function(e)
+        {
+            //remove current list
+            //append original list from request
+            //refresh
+        });
+    $( '#i-attending' ).on( 'click tap', function(e)
+        {
+            var templist;
+            for(blabla)
+            {
+                if(beepboop.filter == '1')
+                {
+                    //add to list
+                }
+            }
+            //remove current list
+            //append new list
+            //refresh
+        });
+    
+    function regenerateList(iCreated,allMeeting,iAttending)
+    {    
+        var templist;
+        for(blablabla)
+        {
+            if(iCreated)
+            {
+                if(bla.filter == '2')
+                {
+                    addMeetingToList(bla);
+                }
+            }
+            if(allMeeting)
+            {
+                while(templist.length != 0)
+                {
+                    removeMeetingfromList(templist[i]);
+                }
+                while(list.length != templist.length)
+                {
+                    addMeetingToList(list[i]);
+                }
+            }
+            if(iAttending)
+            {
+                
+            }
+        }
+    }
+    
+    function addMeetingToList()
+    {
+
+    }
+    
+    function removeMeetingFromList()
+    {
+    
+    }
+    */
+    
+    
+</script>
+<!--End of my-meetings.php-->
