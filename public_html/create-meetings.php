a0d62b35eab5c0ad636a42367562fbb8f5e12ae1
spelka <sebastian.pelka@hotmail.com>
2014-05-20 11:51:38 -0700

    fixed wierd spacing

--- public_html/create-meetings.php
+++ public_html/create-meetings.php
@@ -4,22 +4,22 @@
 
     <div data-role="main" id="create-meeting-div">
         <form id="create-edit-meeting-form" name="create-edit-meeting-form" method="post">
-            <label for="course-dropdown">Course :</label><br/>
+            <label for="course-dropdown">Course :</label>
             <select id="course-dropdown" name="course-dropdown"></select><br/>
 
-            <label for="location-dropdown">Location :</label><br/>
+            <label for="location-dropdown">Location :</label>
             <select id="location-dropdown" name="location-dropdown"></select><br/>
 
-            <label for="meeting-start-datetime">Start Time :</label><br/>
+            <label for="meeting-start-datetime">Start Time :</label>
             <input id="create-meeting-start-datetime" name="meeting-start-datetime" type="text"><br/>
 
-            <label for="meeting-end-datetime">End Time :</label><br/>
+            <label for="meeting-end-datetime">End Time :</label>
             <input id="create-meeting-end-datetime" name="meeting-end-datetime" type="text"><br/>
 
-            <label for="max-buddies">Maximum Buddies :</label><br/>
+            <label for="max-buddies">Maximum Buddies :</label>
             <input id="max-buddies" name="max-buddies" type="number" min="1" max="99"><br/>
 
-            <label for="meeting-comments">Comments :</label><br/>
+            <label for="meeting-comments">Comments :</label>
             <textarea id="meeting-comments" name="meeting-comments"></textarea><br/>
 
         </form>
--- public_html/edit-meetings.php
+++ public_html/edit-meetings.php
@@ -10,22 +10,22 @@
 
     <div data-role="main" id="edit-meeting-div">
         <form id="create-edit-meeting-form" name="create-edit-meeting-form" method="post">
-            <label for="course-dropdown">Course :</label><br/>
+            <label for="course-dropdown">Course :</label>
             <select id="course-dropdown" name="course-dropdown"></select><br/>
 
-            <label for="location-dropdown">Location :</label><br/>
+            <label for="location-dropdown">Location :</label>
             <select id="location-dropdown" name="location-dropdown"></select><br/>
 
-            <label for="meeting-start-datetime">Start Time :</label><br/>
+            <label for="meeting-start-datetime">Start Time :</label>
             <input id="meeting-start-datetime" name="meeting-start-datetime" type="text"><br/>
 
-            <label for="meeting-end-datetime">End Time :</label><br/>
+            <label for="meeting-end-datetime">End Time :</label>
             <input id="meeting-end-datetime" name="meeting-end-datetime" type="text"><br/>
 
-            <label for="max-buddies">Maximum Buddies :</label><br/>
+            <label for="max-buddies">Maximum Buddies :</label>
             <input id="max-buddies" name="max-buddies" type="text"><br/>
 
-            <label for="meeting-comments">Comments :</label><br/>
+            <label for="meeting-comments">Comments :</label>
             <textarea id="meeting-comments" name="meeting-comments"></textarea><br/>
 
         </form>
