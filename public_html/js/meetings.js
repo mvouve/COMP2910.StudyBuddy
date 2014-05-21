834791cf6056a057d4df1289f6454ef9eecc8d9f
spelka <sebastian.pelka@hotmail.com>
2014-05-21 11:35:32 -0700

    finished most of the addMeetingsToList function

--- public_html/js/meetings.js
+++ public_html/js/meetings.js
@@ -285,28 +285,34 @@ function addMeetingToList ( meetingID, meetingCourse, meetingLoc, meetingStartTi
 
 
 
-    //get the element to get meetings added to it
+    //assign the meetings list element to a variable
     var meetingList = document.getElementById( 'my-meetings-list' );
     
     //use createElement() to make a div with data-role="collapsible" to store the course information
     var listElement = document.createElement( "div" );
     listElement.setAttribute( "data-role", "collapsible" );
-    listElement.setAttribute( "class", "list-element" );
+    
+    //set the div id for the list element which will contain all the data for a meeting
+    listElement.setAttribute( "id", "meeting-" + meetingID );
 
 
     //create a header to store main information on a meeting
-    var listHeader = document.createElement("h1");
+    var listHeader = document.createElement( "h1" );
     listHeader.innerHTML( "Course: " + meetingCourse + "<br/>" + "Location: " + meetingLoc + "<br/>" + "Date: " + meetingStartTime );
 
     //create a div element to store detailed/supplementary information on a meeting
-    var listBody = document.createElement("div");
+    var listBody = document.createElement( "div" );
+
+    //create a div container for buttons which will appear at the bottom of the expanded accordion
+    var buttonBar = document.createElement( "div" );
     
     //add information to the meetingList varable as a child node (i guess)
     listElement.appendChild(listHeader);
     listElement.appendChild(listBody);
+    listElement.appendChild(buttonBar);
     meetingList.appendChild(listElement);
 
-    $('.list-element').bind('expand', function () 
+    $('#meeting-' + meetingID).bind( 'expand', function () 
     {
         // need some code here to remove any existing children from the parent list element that may exist from previous expands
         this.empty();
@@ -327,42 +333,88 @@ function addMeetingToList ( meetingID, meetingCourse, meetingLoc, meetingStartTi
                 var meetingDesc = json.description;
                 var meetingEndDate = json.endDate;
                 var meetingMaxBuddies = json.maxBuddies;
-                var meetingBuddies = json.buddies //an array of displayNames
-
-
+                var meetingBuddies = "";
+                
                 for ( var i = 0 ; i<json.buddies.length ; i++ )
                 {
-                    meetingBuddies = "" + meetingBuddies + json.buddies[i] + "<br/>";
+                    meetingBuddies = "" + json.buddies[i] + "<br/>";
                 }
 
-                /*call a function to create meeting details and append them to a parent element
-                    @param meetingDesc the meeting description
-                    @param maxbuddies: the max Number of buddies
-                    @param meetingbuddies: an Array of users attending this meeting */
-                createMeetingDetails( meetingDesc, meetingEndDate, meetingMaxBuddies, meetingBuddies)
+                /* call a function to create meeting details and append them to a parent element */
+                createMeetingDetails( '#meeting-' + meetingID, meetingDesc, meetingEndDate, meetingMaxBuddies, meetingBuddies)
             }
         });
+        //create buttons based on the user's relationship to this meeting
+        if ( meetingFilter == 2 )
+        {
+            //button: edit meeting, button: cancel meeting
+            var editButton = document.createElement( "button" );
+            editButton.innerHTML( "Edit Meeting" );
+            editButton.on( 'click tap', function()
+            {
+                
+            });
+
+            var cancelButton = document.createElement( "button" );
+            cancelButton.innerHTML( "Edit Meeting" );
+            cancelButton.on( 'click tap', function()
+            {
+                
+            });
+
+            buttonBar.appendChild(editButton);
+            buttonBar.appendChild(cancelButton);
+        }
+        else if ( ( meetingFilter == 0 ) && ( meetingCancelled == false ) )
+        {
+            //button: join meeting
+            var joinButton = document.createElement( "button" );
+            joinButton.innerHTML( "Edit Meeting" );
+            joinButton.on( 'click tap', function()
+            {
+                
+            });
+            buttonBar.appendChild(joinButton);
+        }
+        else if ( ( meetingFilter == 1 ) && ( meetingCancelled == false ) )
+        {
+            //button: leave meeting
+            var leaveButton = document.createElement( "button" );
+            leaveButton.innerHTML( "Edit Meeting" );
+            leaveButton.on( 'click tap', function()
+            {
+                
+            });
+            buttonBar.appendChild(leaveButton);
+        }
+
     });
 }
 
 /*call a function to create meeting details and append them to a parent element
-    @param parentID the parent element ID
+    @param meetingIDContainer the parent element ID to which elements created here will be appended
     @param meetingDesc the meeting description
     @param maxbuddies: the max Number of buddies
     @param meetingbuddies: an Array of users attending this meeting */
-function createMeetingDetails( meetingDesc, meetingEndDate, meetingMaxBuddies, meetingBuddies)
+function createMeetingDetails( meetingIDContainer, meetingDesc, meetingEndDate, meetingMaxBuddies, meetingBuddies)
 {
-    var descElement = document.createElement("p");
-    descElement.innerHTML("Description: " + meetingDesc);
+    var containerDiv = document.getElementById( meetingIDContainer );
 
-    var endElement = document.createElement("p");
-    endElement.innerHTML("End Date: " + meetingEndDate);
+    var descElement = document.createElement( "p" );
+    descElement.innerHTML( "Description: " + meetingDesc );
+    containerDiv.appendChild( descElement );
 
-    var maxBuddiesElement = document.createElement("p");
-    maxBuddiesElement.innerHTML("Max Buddies: " + meetingMaxBuddies);
+    var endElement = document.createElement( "p" );
+    endElement.innerHTML( "End Date: " + meetingEndDate );
+    containerDiv.appendChild( endElement );
 
-    var buddiesElement = document.createElement("p");
-    buddiesElement.innerHTML("Buddies: " + "<br/>" + meetingBuddies);
+    var maxBuddiesElement = document.createElement( "p" );
+    maxBuddiesElement.innerHTML( "Max Buddies: " + meetingMaxBuddies );
+    containerDiv.appendChild( maxBuddiesElement );
+
+    var buddiesElement = document.createElement( "p" );
+    buddiesElement.innerHTML( "Buddies: " + "<br/>" + meetingBuddies );
+    containerDiv.appendChild( buddiesElement );
 }
 
 /*This function will check the toggles and add the meetings that match the criteria to the list.
